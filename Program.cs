151276d91dfb8bce23a0c5d7d280168ea74c5ebe
George Ireland <georgewhireland@gmail.com>
2022-01-12 18:34:28 +0000

    Now reads SQL DB file and prints rather than reading from original list object. Added UX console writes

--- Program.cs
+++ Program.cs
@@ -11,29 +11,39 @@ namespace EPPlusExcelToSQLDB
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Deleting Database if exists...");
+
             DeleteDatabase();
-            Console.WriteLine("DB Deleted");
-            Console.ReadLine();
+
+            Console.WriteLine("...Creating new Database...");
+
             CreateDatabase();
+
             ReadCsvFile();
-            //ReadTable();
+
+            PrintDataBase();
         }
+
+
+
+
         private static void ReadCsvFile()
         {
             string dbFile = "URI=FILE:TempDatabase.db";
 
-            Console.WriteLine("What is the file path to your Csv?");
+            Console.WriteLine("What is the file path to your Spreadsheet?");
             string file = Console.ReadLine();
+
+            Console.WriteLine("Populating SQL Database and printing worksheets to console...");
+
             FileInfo fileInfo = new FileInfo(file);
 
             using (ExcelPackage package = new ExcelPackage(file))
             {
+               
+
                 foreach (var workSheet in package.Workbook.Worksheets)
                 {
-                    // Existing code
-
-                    //ExcelWorksheet workSheet = package.Workbook.Worksheets["DriveLogSheet1"];
-
 
                     int totalRows = workSheet.Dimension.Rows;
                     int totalColumns = workSheet.Dimension.Columns;
@@ -63,31 +73,13 @@ namespace EPPlusExcelToSQLDB
                         command.ExecuteNonQuery();
                         connection.Close();
 
-
-                        //Console.WriteLine(myList[k].Description + myList[k].Size + myList[k].Content);
                     }
-                    ConsoleTableBuilder
-                    .From(myList)
-                    .WithTitle("DriveLog ", ConsoleColor.Green, ConsoleColor.Black)
-                    .WithTextAlignment(new Dictionary<int, TextAligntment>
-                        {
-                            {2, TextAligntment.Center}
-                        })
-                      .WithCharMapDefinition(new Dictionary<CharMapPositions, char> {
-                        {CharMapPositions.BottomLeft, '=' },
-                        {CharMapPositions.BottomCenter, '=' },
-                        {CharMapPositions.BottomRight, '=' },
-                        {CharMapPositions.BorderTop, '=' },
-                        {CharMapPositions.BorderBottom, '=' },
-                        {CharMapPositions.BorderLeft, '|' },
-                        {CharMapPositions.BorderRight, '|' },
-                        {CharMapPositions.DividerY, '|' },
-                        {CharMapPositions.DividerX, '-' },
-                        })
 
-                    .ExportAndWriteLine();
+                    
 
-                    Console.WriteLine("");
+                    
+
+                    
                 }
             }
 
@@ -109,10 +101,69 @@ namespace EPPlusExcelToSQLDB
 
         static void DeleteDatabase()
         {
+
+            //add location of DB file here before running (after running first time)
             File.Delete(@"C:\Users\georg\Documents\CSharp\EPPlusExcelToSQLDB\TempDatabase.db");
         }
 
-        
+        static void PrintDataBase()
+        {
+            string dbFile = "URI=FILE:TempDatabase.db";
+            var connection = new SQLiteConnection(dbFile);
+
+            connection.Open();
+
+            var tableCmd = connection.CreateCommand();
+            tableCmd.CommandText = "SELECT * FROM tempdatabase";
+
+            List<Item> tableData = new List<Item>();
+
+            SQLiteDataReader reader = tableCmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    tableData.Add(
+                    new Item
+                    {
+                        Description = new string(reader.GetString(0)),
+                        Content = new string(reader.GetString(1)),
+                        Size = new string(reader.GetString(2))
+                    });
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
+            reader.Close();
+
+            ConsoleTableBuilder
+                .From(tableData)
+                .WithTitle("DriveLogs", ConsoleColor.Green, ConsoleColor.Black)
+                .WithTextAlignment(new Dictionary<int, TextAligntment>
+                    {
+                            {2, TextAligntment.Center}
+                    })
+                  .WithCharMapDefinition(new Dictionary<CharMapPositions, char> {
+                        {CharMapPositions.BottomLeft, '=' },
+                        {CharMapPositions.BottomCenter, '=' },
+                        {CharMapPositions.BottomRight, '=' },
+                        {CharMapPositions.BorderTop, '=' },
+                        {CharMapPositions.BorderBottom, '=' },
+                        {CharMapPositions.BorderLeft, '|' },
+                        {CharMapPositions.BorderRight, '|' },
+                        {CharMapPositions.DividerY, '|' },
+                        {CharMapPositions.DividerX, '-' },
+                    })
+
+                .ExportAndWriteLine();
+
+            Console.WriteLine("");
+        }
+
+
     }
     public class Item
     {
@@ -121,11 +172,6 @@ namespace EPPlusExcelToSQLDB
         public string Content { get; set; }
     }
 
-    public class Item2
-    {
-        public string Description2 { get; set; }
-        public string Size2 { get; set; }
-        public string Content2 { get; set; }
-    }
+   
 }
 
